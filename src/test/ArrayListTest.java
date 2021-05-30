f88cdc5c59bae33cc75701b2f1bc267b7932a822
masakulayou <masakulayou@163.com>
2021-05-30 14:25:40 +0800

    upload jdk1.8.0_271 and setup test environment

---
+++ src/test/ArrayListTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: Cheers Lee
+ * Date: 2021/5/30 13:34
+ * Version: 1.0
+ * <p>
+ * ClassName: ArrayListTest
+ * Description: TODO
+ */
+public class ArrayListTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+}
